ef6b4ab36a588e78cdecbca89e7700d1167f8385
Nipun Bahri <bahri.nipun@gmail.com>
2023-01-03 11:29:40 +0100

    updates

--- src/tests/test_container_create.py
+++ src/tests/test_container_create.py
@@ -3,3 +3,7 @@ import unittest
 class TestContainerCreation(unittest.TestCase):
     def TestContainerCreate(self):
         self.assertEqual('foo'.upper(),'FOO')
+
+
+if __name__ == '__main__':
+    unittest.main()
