82619d46c75508d72fc406915880fe88c934266a
Nipun Bahri <bahri.nipun@gmail.com>
2023-01-03 11:59:58 +0100

    updates

--- src/tests/test_container_create.py
+++ src/tests/test_container_create.py
@@ -1,9 +1,25 @@
 import unittest
+import azure.functions as func
+from container_create import main
+import json
 
 
 class Test_ContainerCreation(unittest.TestCase):
-    def test_container_create(self):
+    def test_sample(self):
         self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_container_create(self):
+        req = func.HttpRequest(
+            method='POST',
+            body=None,
+            url='/api/storage/container')
+
+        # resp = main(req)
+
+        # self.assertEqual(
+        #     resp.get_body(),
+        #     json.dumps({'reason': 'UnAuthorized'}).encode(),
+        # )
 
 
 if __name__ == '__main__':
