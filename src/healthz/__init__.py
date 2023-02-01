bfa888385d69f8ebbe5a8013b5e8bf7357abb982
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2023-02-01 14:16:00 +0100

    Update __init__.py

--- src/healthz/__init__.py
+++ src/healthz/__init__.py
@@ -1,1 +1,9 @@
 
+import json
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> str:
+      
+    return func.HttpResponse(json.dumps({ 'reason' : 'functionapp is up & running!' }), mimetype="application/json",status_code=200)
+
