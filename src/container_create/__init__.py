e228666af45f4d1da40c9480237253bcd3153ff5
Nipun Bahri <bahri.nipun@gmail.com>
2023-02-04 11:10:10 +0100

    ADD

--- src/container_create/__init__.py
+++ src/container_create/__init__.py
@@ -16,9 +16,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hi,{name}.triggered function executed.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "Pass a name in the query string or personalized response.",
              status_code=200
         )
