d57aa088fdbe91a2642e3d7b44c4621894d2c7a0
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2023-03-20 14:45:51 +0100

    Update auth_helper.py

--- src/shared/helpers/auth_helper.py
+++ src/shared/helpers/auth_helper.py
@@ -4,6 +4,16 @@ import base64
 
 class AuthHelper():
   
+def get_credentials(self) -> DefaultAzureCredential:
+  ''' Acquire a credential object for the app identity. 
+  When running on Azure: DefaultAzureCredential uses the app's managed identity (MSI) or user-assigned service principal.
+  When running on your a local host: DefaultAzureCredential relies on environment variables named AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, and AZURE_TENANT_ID. 
+
+  Create your own SPN for local development and add these values to you local.settings.json
+  https://docs.microsoft.com/en-gb/cli/azure/create-an-azure-service-principal-azure-cli'''
+
+  return DefaultAzureCredential()
+  
 def is_authorized(self, req, ignore_exp = False):
   
   for header in req.headers:
