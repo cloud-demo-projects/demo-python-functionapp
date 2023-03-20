4cb4c865d8074febd724348849fb5890b3be7d14
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2023-03-20 14:25:06 +0100

    Update auth_helper.py

--- src/shared/helpers/auth_helper.py
+++ src/shared/helpers/auth_helper.py
@@ -7,12 +7,17 @@ class AuthHelper():
 def is_authorized(self, req, ignore_exp = False):
   
   for header in req.headers:
-        logging.info(f"header-{header}")
+    logging.info(f"header-{header}")
+      
+  auth_header = req.headers.get('Authorization')
+  if not auth_header:
+    logging.error("Authorization header Not found in the request")
+    return False
 
-    if req.headers.get('X-MS-CLIENT-PRINCIPAL') != None:
-        clientPrincipal = req.headers.get('X-MS-CLIENT-PRINCIPAL') # Extract client principal from header
-        logging.info(f"clientPrincipal-{clientPrincipal}")
-        cp = json.loads(base64.b64decode(clientPrincipal).decode('utf-8')) # Decode client principal
-        logging.info(f"cp-{cp}")
-        roles = [x['val'] for x in cp['claims'] if x['typ'] == 'roles'] # Extract claims from client principal
-        logging.info(f"roles-{roles}")
+  if auth_header.get('X-MS-CLIENT-PRINCIPAL') != None:
+    clientPrincipal = req.headers.get('X-MS-CLIENT-PRINCIPAL') # Extract client principal from header
+    logging.info(f"clientPrincipal-{clientPrincipal}")
+    cp = json.loads(base64.b64decode(clientPrincipal).decode('utf-8')) # Decode client principal
+    logging.info(f"cp-{cp}")
+    roles = [x['val'] for x in cp['claims'] if x['typ'] == 'roles'] # Extract claims from client principal
+    logging.info(f"roles-{roles}")
