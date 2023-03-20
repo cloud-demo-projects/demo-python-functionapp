2da5fdf034cad50e44e158b7f1c263f62b7c6964
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2023-03-20 14:19:32 +0100

    Create enums.py

---
+++ src/shared/enums/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class ProvisioningStatus(Enum):
+   'Provisioning statuses'
+   Done = 'Done'
+   Failed = 'Failed'
+   Other = 'Other'
+   
+class HttpStatusReasons(Enum):
+   'HTTP messages reasons'
+   Unauthorized = 'The client application does not have permissions to call the API.'
+   OkHealthz = 'This function app method is up & running'
+   NotImplemented = 'This function app method is still being worked!'
+   InternalServerError = 'An error ocurred, please check App Insights logs to get further information.'
