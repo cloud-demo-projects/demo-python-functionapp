7e970ca51107319ec1c9e26f7e15fcd19dac1ae1
Nipun Bahri <bahri.nipun@gmail.com>
2023-03-20 21:00:46 +0100

    updates

--- src/container_create/__init__.py
+++ src/container_create/__init__.py
@@ -5,6 +5,9 @@ from azure.storage.blob import BlobServiceClient
 from shared.helpers.log_helper import LogHelper
 from shared.helpers.auth_helper import AuthHelper
 from shared.models.request_payload import CoreProducer
+from shared.models.response_payload import ContainerProvisioningStatus
+from shared.enums import HttpStatusReasons
+from shared.enums import ProvisioningStatus
 import json
 import base64
 import os
@@ -18,6 +21,7 @@ RUN_ID = str(uuid.uuid4())
 log_helper = LogHelper(RUN_ID)
 auth_helper = AuthHelper()
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -26,6 +30,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(json.dumps({ 'reason' : HttpStatusReasons.Unauthorized.value }), mimetype="application/json",status_code=401)
 
     return run(req)
+
 
 def run(req: func.HttpRequest) -> str:
     log_helper.log_info("Run function execution started")
@@ -40,32 +45,32 @@ def run(req: func.HttpRequest) -> str:
     except ValueError as e:
         log_helper.log_error(f"{e}")
         log_helper.log_error(BAD_REQUEST_MESSAGE)
-        return func.HttpResponse(str(e), status_code=400) 
+        return func.HttpResponse(str(e), status_code=400)
     except ValidationError as e:
         log_helper.log_error(f"{e}")
         log_helper.log_error(BAD_REQUEST_MESSAGE)
         return func.HttpResponse(e.json(), status_code=400)
-    
+
     response.containers.append(create(producer.ContainerName, producer.StorageAccountName))
-    
+
     log_helper.log_info("Function execution finished with status 200 (OK)")
     log_helper.log_info(f"Response body: {json.dumps(response.__dict__, default = lambda o: o.__dict__,sort_keys=True)}")
-    
+
     # return func.HttpResponse(f"{container_name} container created.", status_code=200)
     return func.HttpResponse(
         json.dumps(response.__dict__, default = lambda o: o.__dict__,sort_keys=True),
         mimetype="application/json",
         status_code=200)
-    
-    
+
+
 def create(container_name, storage_account_name) -> ContainerProvisioningStatus:
     log_helper.log_info(f"Started container {container_name} creation on {storage_account_name}")
-    
+
     # storage_account_name = "adls12133"
     # default_credential = DefaultAzureCredential()
     default_credential = auth_helper.get_credentials()
     cps = ContainerProvisioningStatus(storage_account_name,container_name)
-    
+
     blob_service_client = BlobServiceClient(account_url="{}://{}.blob.core.windows.net".format(
         "https", storage_account_name), credential=default_credential)
 
@@ -83,5 +88,5 @@ def create(container_name, storage_account_name) -> ContainerProvisioningStatus:
         blob_service_client.close()
 
     log_helper.log_info(f"Finished container creation- {container_name}")
-    
+
     return cps
