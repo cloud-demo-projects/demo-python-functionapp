14d0222ca40b18d688b51a81e17a1c863de4e49c
Nipun Bahri <67367858+BahriNipun@users.noreply.github.com>
2023-07-09 21:06:47 +0200

    Update setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,28 +1,45 @@
--- TODO
-
+-- Prerequisites
 -- Database server creation with SPN as databaserver admin
 -- Database server firewall rules creation
 -- Database and schema ceation
 
 -- Database schema ceation
-Create schema [core]
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.schemas
+    WHERE name = 'core'
+)
+    BEGIN
+        EXEC sp_executesql N'CREATE SCHEMA [core];';
+    END
 
 -- Database role creation
-Create role [MSI]
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.database_principals
+    WHERE name = 'developer'
+)
+    BEGIN
+        CREATE ROLE [developer] AUTHORIZATION [dbo];
+    END;
 
 -- Database users(MSI and Engineers) creation
-Create user [MSI] as external user
-Create user [AD-Group] as external user
+IF NOT EXISTS (
+    SELECT *
+    FROM sys.database_principals
+    WHERE name = 'testuser'
+)
+    BEGIN
+        CREATE USER [testuser] FROM EXTERNAL PROVIDER ;
+    END
 
 -- Database permissions creation
-Grant select on schema::[MSI] to [MSI]
-Grant select on schema::[MSI] to [Engineers]
-Grant insert on schema::[MSI] to [Engineers]
-Grant update on schema::[MSI] to [Engineers]
-Grant delete on schema::[MSI] to [Engineers]
+GRANT INSERT ON SCHEMA::[core] TO [developer];
+GRANT UPDATE ON SCHEMA::[core] TO [developer];
+GRANT DELETE ON SCHEMA::[core] TO [developer];
 
 -- Database tables creation
-Create table [MSI].[dbo].[MSI] (
+Create table [core].[dbo].[MyTable] (
     [Id] int identity(1,1) not null,
     [Name] nvarchar(50) not null,
     [Description] nvarchar(100) not null,
